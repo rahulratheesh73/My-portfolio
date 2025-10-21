69d80583710dbff4221a4166f87005f216b4d31d
rahulratheesh0 <rahul@purposefintech.com>
2025-10-21 22:20:31 +0530

    add new cursor

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,12 +4,15 @@ import About from "@/components/Portfolio/About";
 import Skills from "@/components/Portfolio/Skills";
 import Projects from "@/components/Portfolio/Projects";
 import Contact from "@/components/Portfolio/Contact";
-import MouseFollower from "@/components/Portfolio/MouseFollower";
+// import MouseFollower from "@/components/Portfolio/MouseFollower";
+import SplashCursor from '@/components/SplashCursor'
 
+<SplashCursor />
 const Index = () => {
   return (
     <div id="home" className="min-h-screen bg-background">
-      <MouseFollower />
+      {/* <MouseFollower /> */}
+      <SplashCursor />
       <Navigation />
       <main>
         <Hero />
